6904a2be72e6c7bb6234892f1439182b33d3c3bc
Karina <karycardenaspastrana@gmail.com>
2018-05-05 12:47:44 -0500

    Ejemplo Maven

---
+++ src/main/java/com/politecnica/workshops/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.politecnica.workshops;
+
+public class HelloWorld{
+    public static final java.lang.String HELLO_WORLD = "Hello World!";
+
+    public static void main(String[] args){
+        System.out.println(HELLO_WORLD);
+    }
+}
---
+++ src/main/java/com/politecnica/workshops/Operation.java
@@ -0,0 +1,8 @@
+package com.politecnica.workshops;
+
+public class Operation {
+
+    public int suma(int a, int b){
+        return a+b;
+    }
+}
